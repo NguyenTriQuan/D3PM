19dba9aa61a2666d9d649fe32e79b511a3789ea7
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-08-18 11:11:58 +0700

    diff

--- run_condition.sh
+++ run_condition.sh
@@ -1,4 +1,4 @@
-python -m torch.distributed.launch --nproc_per_node 1 --master_port 29500 DDP_main_conditional.py \
+python -m torch.distributed.launch --nproc_per_node 1 --master_port 29500 --use_env DDP_main_conditional.py \
   --lr 5e-5 \
   --batch_size 64 \
   --task_name 'QT' \
