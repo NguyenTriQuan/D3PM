89ec690ee90a736039c8603946746fbfc57c42b1
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-08-18 11:07:04 +0700

    diff

--- run_condition.sh
+++ run_condition.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES="0,1,2,3" python -m torch.distributed.launch --nproc_per_node 4 --master_port 29500 DDP_main_conditional.py \
+CUDA_VISIBLE_DEVICES="0,3,4" python -m torch.distributed.launch --nproc_per_node 3 --master_port 29500 DDP_main_conditional.py \
   --lr 5e-5 \
   --batch_size 64 \
   --task_name 'QT' \
