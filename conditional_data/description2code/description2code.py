319dc9ca5e1a3ec64e1040f90d6bb8668c1b2de5
Nguyễn Trí Quân <nguyentriquan99@gmail.com>
2023-08-25 11:18:54 +0700

    diff

--- conditional_data/description2code/description2code.py
+++ conditional_data/description2code/description2code.py
@@ -23,11 +23,10 @@ class D2C(datasets.GeneratorBasedBuilder):
         )
 
     def _split_generators(self, dl_manager):
-        output_file = 'conditional_data/description2code/description2code.zip' 
-        # url = 'https://drive.google.com/drive/u/0/folders/1CNmq-ANr_FNw2o7Y-zSrFvQsSKX0CvGQ'
-        url = 'https://drive.google.com/file/d/1UEqP1GpaIS2cSoVwKDezR2HqE3RUv-MT/view?usp=sharing'
-        gdown.download(url, output_file, quiet=False)
-        with zipfile.ZipFile(output_file, 'r') as zip_ref:
+        data_dir = 'conditional_data/description2code' 
+        url = 'https://drive.google.com/drive/folders/1CNmq-ANr_FNw2o7Y-zSrFvQsSKX0CvGQ'
+        gdown.download_folder(url, output=data_dir, quiet=True, use_cookies=False)
+        with zipfile.ZipFile('conditional_data/description2code/data/description2code_current.zip', 'r') as zip_ref:
             # Extract all the contents to the destination folder
             zip_ref.extractall('conditional_data/description2code')
 
